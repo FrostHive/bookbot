0645a3e75203ddaa43d7bc66dbcb55381cd5143b
FrostHive <alfrobusiness@gmail.com>
2025-09-02 18:47:25 -0500

    Added new functions and formatting to existing python scripts

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sort_dictionary
+import sys
 
 def main():
+
     book_text = get_book_text("./books/frankenstein.txt")
     word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
-    print(get_char_count(book_text))
+    char_count = sort_dictionary(get_char_count(book_text))
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+    print ("--------- Character Count -------")
+    for char in char_count:
+        print(f"{char['char']}: {char['num']}")
+
+    print("============= END ===============")
 
 def get_book_text(file_location):
     with open(file_location) as f:
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def get_char_count(book_text):
         else:
             char_dict[char] = 1
     return char_dict
+
+def sort_key(char_dictionary):
+    return char_dictionary["num"]
+
+def sort_dictionary(dictionary):
+    sorted_list = []
+    for char in dictionary:
+        if(char.isalpha()):
+            temp_dictionary = {"char": char, "num": dictionary[char]}
+            sorted_list.append(temp_dictionary)
+
+    sorted_list.sort(reverse=True, key=sort_key)
+    return sorted_list
