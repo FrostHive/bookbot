fe5ed4ba4e06a14a9bf7ba4bdc7ecd9989d7ae38
FrostHive <alfrobusiness@gmail.com>
2025-09-02 19:01:57 -0500

    Added functionality for custom input for main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from stats import get_word_count, get_char_count, sort_dictionary
 import sys
 
 def main():
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        print(f"\nError: 2 arguments are needed for running main.py. {len(sys.argv)} was given")
+        sys.exit(1)
 
-    book_text = get_book_text("./books/frankenstein.txt")
+
+    book_text = get_book_text(sys.argv[1])
     word_count = get_word_count(book_text)
     char_count = sort_dictionary(get_char_count(book_text))
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
 
